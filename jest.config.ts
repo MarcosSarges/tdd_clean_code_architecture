a0fab5afc96dc1da36ef66589435734580de3749
Marcos Sarges <dev.marcos.sarges@gmail.com>
2021-11-24 08:58:23 -0300

    start project clean architecture, clean code and automated tests

---
+++ jest.config.ts
@@ -0,0 +1,8 @@
+export default {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
+  testPathIgnorePatterns: ['/node_modules/'],
+};
