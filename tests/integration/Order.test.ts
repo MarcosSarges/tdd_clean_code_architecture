636405eca62787f6faad2ff96c4489a7b1a9e37d
Marcos Sarges <dev.marcos.sarges@gmail.com>
2022-01-12 10:51:02 -0300

    refactor folders

--- tests/integration/Order.test.ts
+++ tests/integration/Order.test.ts
@@ -1,7 +1,3 @@
-// 1 - Não deve fazer um pedido com cpf inválido
-// 2 - Deve fazer um pedido com 3 itens (com descrição, preço e quantidade)
-// 3 - Deve fazer um pedido com cupom de desconto (percentual sobre o total do pedido)
-
 import Cart from '../../src/entities/Cart';
 import Cupom from '../../src/entities/Cupom';
 import Item from '../../src/entities/Item';
