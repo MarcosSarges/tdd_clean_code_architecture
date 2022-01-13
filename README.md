ffb28688c53fe824cc2965f4c6cc22f89d251d0b
Marcos Sarges <dev.marcos.sarges@gmail.com>
2022-01-13 14:09:41 -0300

    build(test): okay esta funcionando

--- README.md
+++ README.md
@@ -4,7 +4,5 @@
 ![Functions](/badges/badge-functions.svg)
 ![Lines](/badges/badge-lines.svg)
 ![Statements](/badges/badge-statements.svg)
-
-[![codecov](https://codecov.io/gh/MarcosSarges/tdd_clean_code_architecture/branch/master/graph/badge.svg?token=XT93M8F2Z3)](https://codecov.io/gh/MarcosSarges/tdd_clean_code_architecture)
-
-[![Commitizen friendly](https://img.shields.io/badge/commitizen-friendly-brightgreen.svg)](http://commitizen.github.io/cz-cli/)
+![![codecov](https://codecov.io/gh/MarcosSarges/tdd_clean_code_architecture/branch/master/graph/badge.svg?token=XT93M8F2Z3)](https://codecov.io/gh/MarcosSarges/tdd_clean_code_architecture)
+![![Commitizen friendly](https://img.shields.io/badge/commitizen-friendly-brightgreen.svg)](http://commitizen.github.io/cz-cli/)
--- tests/unit/User.test.ts
+++ tests/unit/User.test.ts
@@ -17,63 +17,63 @@ describe('User', () => {
     }).toThrowError('CPF INVALIDO');
   });
 
-  // describe('Validator cpf', () => {
-  //   let user: User;
+  describe('Validator cpf', () => {
+    let user: User;
 
-  //   beforeEach(() => {
-  //     user = new User();
-  //   });
+    beforeEach(() => {
+      user = new User();
+    });
 
-  //   it('Deve validar o cpf sem mascara e retorna valido', () => {
-  //     const CPF_VALIDO = '111.444.777-35';
+    it('Deve validar o cpf sem mascara e retorna valido', () => {
+      const CPF_VALIDO = '111.444.777-35';
 
-  //     expect(user.cpfValidator(CPF_VALIDO)).toBeTruthy();
-  //   });
-  //   it('Deve validar o cpf e passar no caso o ultimo digito for 0', () => {
-  //     const CPF_VALIDO = '84260727710';
+      expect(user.cpfValidator(CPF_VALIDO)).toBeTruthy();
+    });
+    it('Deve validar o cpf e passar no caso o ultimo digito for 0', () => {
+      const CPF_VALIDO = '84260727710';
 
-  //     expect(user.cpfValidator(CPF_VALIDO)).toBeTruthy();
-  //   });
-  //   it('Deve validar o cpf e passar no caso o penultimo digito for 0', () => {
-  //     const CPF_VALIDO = '21415160805';
-  //     expect(user.cpfValidator(CPF_VALIDO)).toBeTruthy();
-  //   });
+      expect(user.cpfValidator(CPF_VALIDO)).toBeTruthy();
+    });
+    it('Deve validar o cpf e passar no caso o penultimo digito for 0', () => {
+      const CPF_VALIDO = '21415160805';
+      expect(user.cpfValidator(CPF_VALIDO)).toBeTruthy();
+    });
 
-  //   it('Deve validar o cpf com mascara e retorna valido', () => {
-  //     const CPF_VALIDO = '031.076.252-99';
+    it('Deve validar o cpf com mascara e retorna valido', () => {
+      const CPF_VALIDO = '031.076.252-99';
 
-  //     expect(user.cpfValidator(CPF_VALIDO)).toBeTruthy();
-  //   });
+      expect(user.cpfValidator(CPF_VALIDO)).toBeTruthy();
+    });
 
-  //   it('Deve retornar que o cpf é invalido', () => {
-  //     const CPF_INVALIDO = '03107625202';
-  //     expect(user.cpfValidator(CPF_INVALIDO)).not.toBeTruthy();
-  //   });
-  //   it('Deve retornar que o cpf é invalido para caso a string tenha numeros repetidos', () => {
-  //     const CPF_INVALIDO = '111.111.111-11';
-  //     expect(user.cpfValidator(CPF_INVALIDO)).not.toBeTruthy();
-  //   });
+    it('Deve retornar que o cpf é invalido', () => {
+      const CPF_INVALIDO = '03107625202';
+      expect(user.cpfValidator(CPF_INVALIDO)).not.toBeTruthy();
+    });
+    it('Deve retornar que o cpf é invalido para caso a string tenha numeros repetidos', () => {
+      const CPF_INVALIDO = '111.111.111-11';
+      expect(user.cpfValidator(CPF_INVALIDO)).not.toBeTruthy();
+    });
 
-  //   it('Deve retornar false caso o input seja null', () => {
-  //     const CPF_INVALIDO = null;
-  //     expect(user.cpfValidator(CPF_INVALIDO)).not.toBeTruthy();
-  //   });
+    it('Deve retornar false caso o input seja null', () => {
+      const CPF_INVALIDO = null;
+      expect(user.cpfValidator(CPF_INVALIDO)).not.toBeTruthy();
+    });
 
-  //   it('Deve retornar false caso o input seja undefined', () => {
-  //     const CPF_INVALIDO = undefined;
-  //     expect(user.cpfValidator(CPF_INVALIDO)).not.toBeTruthy();
-  //   });
-  //   it('Deve retornar false caso o cpf tenha menos que 11 caracteres', () => {
-  //     const CPF_INVALIDO = '123456789';
-  //     expect(user.cpfValidator(CPF_INVALIDO)).not.toBeTruthy();
-  //   });
-  //   it('Deve retornar false caso o cpf tenha mais que 14 caracteres', () => {
-  //     const CPF_INVALIDO = '123.456.789-091';
-  //     expect(user.cpfValidator(CPF_INVALIDO)).not.toBeTruthy();
-  //   });
-  //   it('Deve retornar false caso o cpf tenha letras', () => {
-  //     const CPF_INVALIDO = '123.456.789-av';
-  //     expect(user.cpfValidator(CPF_INVALIDO)).not.toBeTruthy();
-  //   });
-  // });
+    it('Deve retornar false caso o input seja undefined', () => {
+      const CPF_INVALIDO = undefined;
+      expect(user.cpfValidator(CPF_INVALIDO)).not.toBeTruthy();
+    });
+    it('Deve retornar false caso o cpf tenha menos que 11 caracteres', () => {
+      const CPF_INVALIDO = '123456789';
+      expect(user.cpfValidator(CPF_INVALIDO)).not.toBeTruthy();
+    });
+    it('Deve retornar false caso o cpf tenha mais que 14 caracteres', () => {
+      const CPF_INVALIDO = '123.456.789-091';
+      expect(user.cpfValidator(CPF_INVALIDO)).not.toBeTruthy();
+    });
+    it('Deve retornar false caso o cpf tenha letras', () => {
+      const CPF_INVALIDO = '123.456.789-av';
+      expect(user.cpfValidator(CPF_INVALIDO)).not.toBeTruthy();
+    });
+  });
 });
